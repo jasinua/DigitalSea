6a53f7c8c05e359c25ff06e51f806d6f5b08d2a1
jasinua1 <jasin.avdiu@student.uni-pr.edu>
2025-03-26 14:15:25 +0100

    ndryshime
    ndryshimeee

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,12 +4,11 @@ $username = "sql7769680";
 $password = "6egMd5J3XE";      
 $dbname = "sql7769680";  
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
